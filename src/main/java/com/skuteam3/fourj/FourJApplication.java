8e6e52d6d76931143d91fd67f1f00bdc3df59d35
MeongW <easymiracle@skuniv.ac.kr>
2024-07-18 22:26:30 +0900

    프로젝트 세팅

---
+++ src/main/java/com/skuteam3/fourj/FourJApplication.java
@@ -0,0 +1,13 @@
+package com.skuteam3.fourj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skuteam3/fourj/FourJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skuteam3.fourj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
