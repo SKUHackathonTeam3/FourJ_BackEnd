5bb24c3194c3e4ecda62abdb0438759996344420
MeongW <easymiracle@skuniv.ac.kr>
2024-07-29 02:22:06 +0900

    Feature/#23 Challenge 스웨거 정보 추가 및 수정

--- src/main/java/com/skuteam3/fourj/challenge/controller/ChallengeController.java
+++ src/main/java/com/skuteam3/fourj/challenge/controller/ChallengeController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@Tag(name="challenges", description="금주, 절주 챌린지 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/challenges")
 @RestController
