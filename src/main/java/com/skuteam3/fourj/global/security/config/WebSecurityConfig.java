aed23c71ff30e0e664850fd87f18e14d03ba3c7b
MeongW <125392666+MeongW@users.noreply.github.com>
2024-08-05 21:59:16 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/skuteam3/fourj/global/security/config/WebSecurityConfig.java
+++ src/main/java/com/skuteam3/fourj/global/security/config/WebSecurityConfig.java
@@ -103,8 +103,7 @@ public class WebSecurityConfig {
                         "http://localhost:5173",
                         "http://localhost:8080",
                         "http://ec2-43-201-61-252.ap-northeast-2.compute.amazonaws.com:8080/",
-                        "https://api.smartcheers.site",
-                        "https://smartcheers.site"
+                        "https://api.smartcheers.site"
                 )
         );
         corsConfiguration.setAllowedHeaders(
